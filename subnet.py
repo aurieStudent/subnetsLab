a3050a1ced4429b2463b28779fb9de4bd9ff58d8
aurieStudent <144954692+aurieStudent@users.noreply.github.com>
2024-03-26 00:57:37 -0400

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 60621
+
+#problem 1  #187.34.178.61, CIDR: 26
+p1Network = '187.34.178.0'
+p1Broadcast = '187.34.178.63'
+p1First = '187.34.178.1'
+p1Last = '187.34.178.62'
+
+#problem 2 #98.20.140.104, CIDR: 29
+p2Network = '98.20.140.104'
+p2Broadcast = '98.20.140.111'
+p2First = '98.20.140.105'
+p2Last = '98.20.140.110'
+
+#problem 3 #9.5.102.146, CIDR: 27
+p3Network = '9.5.102.128'
+p3Broadcast = '9.5.102.159'
+p3First = '9.5.102.129'
+p3Last = '9.5.102.158'
+
+#problem 4 #176.246.63.189, CIDR: 24
+p4Network = '176.246.63.0'
+p4Broadcast = '176.246.63.255'
+p4First = '176.246.63.1'
+p4Last = '198.106.60.254'
+
+#problem 5 #87.231.25.231, CIDR: 27
+p5Network = '87.231.25.224'
+p5Broadcast = '87.231.25.255'
+p5First = '87.231.25.225'
+p5Last = '87.231.25.254'
+
+#problem 6 #254.216.243.18, CIDR: 25
+p6Network = '254.216.243.0'
+p6Broadcast = '254.216.243.127'
+p6First = '254.216.243.1'
+p6Last = '254.216.243.126'
+
+#problem 7 #165.201.205.60, CIDR: 28
+p7Network = '165.201.205.48'
+p7Broadcast = '165.201.205.63'
+p7First = '165.201.205.49'
+p7Last = '165.201.205.62'
+
+#problem 8 #76.186.166.103, CIDR: 29
+p8Network = '76.186.166.96'
+p8Broadcast = '76.186.166.103'
+p8First = '76.186.166.97'
+p8Last = '76.186.166.102'
+
+#problem 9 #243.172.128.145, CIDR: 29
+p9Network = '243.172.128.144'
+p9Broadcast = '243.172.128.151'
+p9First = '243.172.128.145'
+p9Last = '243.172.128.140'
+
+#problem 10 #154.157.90.187, CIDR: 27
+p10Network = '154.157.90.160'
+p10Broadcast = '154.157.90.191'
+p10First = '154.157.90.161'
+p10Last = '154.157.90.190'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
